77fdd11566badb5b860bb23362968d9b1d183783
zzzRYT <jinjinstar3@gmail.com>
2024-12-26 17:34:19 +0900

    setting/LJJ(#2) : 주석문 추가

--- src/provider/ThemeColorProvider.tsx
+++ src/provider/ThemeColorProvider.tsx
@@ -3,6 +3,20 @@ import { mainThemeColor } from '../styles/theme';
 
 /**
  * 테마지정을 위한 Provider
+ *
+ * @example
+ * import { theme } from "테마 경로"
+ *
+ * const ThemeTest = styled.div`
+ *  background-color: ${({ theme }) => theme.main_100};
+ *  color: ${({ theme }) => theme.text_100};
+ * `
+ *
+ * @example
+ * import { useTheme } from "styled-components"
+ *
+ * const theme = useTheme()
+ * ...
  */
 export default function ThemeColorProvider({
   children,
