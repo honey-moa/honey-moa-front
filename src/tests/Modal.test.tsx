75f0b3b427438ffd42172d7273a9980733867540
woo <pho9902@naver.com>
2024-12-28 16:36:05 +0900

    feat/woo(#1): vitest, react-testing-library 설치, modal 테스트 추가

---
+++ src/tests/Modal.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import Modal from '../components/Modal';
+import { expect, describe, beforeAll, it } from 'vitest';
+
+beforeAll(() => {
+  render(
+    <div>
+      <button onClick={() => console.log(1)}>Click</button>
+      <Modal isOpen={true} data-testid="outside">
+        <div>
+          <h1>testHeader</h1>
+          <div>
+            <p>testBody</p>
+          </div>
+        </div>
+      </Modal>
+    </div>
+  );
+});
+describe('Modal test', () => {
+  it('1. isOpen 값을 true 로 전달하면 Modal 창이 열린다.', () => {
+    const modalHeader = screen.getByText('testHeader');
+
+    expect(modalHeader).toBeDefined();
+  });
+  it('2. Modal 영억 내부를 클릭해도 Modal 창은 닫히지 않는다.', () => {
+    const modalHeader = screen.getByText('testHeader');
+
+    fireEvent.click(modalHeader);
+
+    expect(modalHeader).toBeDefined();
+  });
+  //추후 추가
+  // it('3. Modal 영역 외부를 클릭하면 Modal 창이 닫힌다.', () => {
+  // });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  //@ts-ignore
+  test: {
+    browser: {
+      enabled: true,
+      name: 'chromium', // 크롬 브라우저 사용중
+    },
+  },
+});
