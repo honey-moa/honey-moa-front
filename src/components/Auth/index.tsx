59053a5cbb727883031ca822edb527cfbd1b821d
zzzRYT <jinjinstar3@gmail.com>
2025-01-01 17:18:00 +0900

    feat/LJJ(#17): Modal -> Funnel로 변경

--- src/components/Auth/index.tsx
+++ src/components/Auth/index.tsx
@@ -2,40 +2,40 @@ import { useState } from 'react';
 import * as S from './style';
 import LoginModal from './LoginModal';
 import RegisterModal from './RegisterModal';
-import { modalLocation } from './auth';
+import Modal from '../Modal';
+import useFunnel from '../../hook/useFunnel';
+import { SelectModalButton } from './type';
 
 export default function Auth() {
-  const [loginOpen, setLoginOpen] = useState<boolean>(false);
-  const [registerOpen, setRegisterOpen] = useState<boolean>(false);
+  const [authOpen, setAuthOpen] = useState<boolean>(false);
+  const { Funnel, setStep } = useFunnel<'로그인' | '회원가입'>('로그인');
 
-  const onClickModalButton = (location: modalLocation) => {
-    if (location === 'login') {
-      setLoginOpen(prev => !prev);
-    } else {
-      setRegisterOpen(prev => !prev);
-    }
-  };
-
-  const onClickOtherModalOpen = (location: modalLocation) => {
-    if (location === 'login') {
-      setLoginOpen(prev => !prev);
-      setRegisterOpen(prev => !prev);
-    } else {
-      setLoginOpen(prev => !prev);
-      setRegisterOpen(prev => !prev);
-    }
+  const onClickModalButton = ({ location }: SelectModalButton) => {
+    if (location === '로그인') setStep('로그인');
+    if (location === '회원가입') setStep('회원가입');
+    setAuthOpen(prev => !prev);
   };
 
   return (
     <>
-      <S.LoginButton onClick={() => onClickModalButton('login')}>
+      <S.LoginButton onClick={() => onClickModalButton({ location: '로그인' })}>
         로그인
       </S.LoginButton>
-      <LoginModal isOpen={loginOpen} onClose={onClickOtherModalOpen} />
-      <S.RegisterButton onClick={() => onClickModalButton('register')}>
+      <S.RegisterButton
+        onClick={() => onClickModalButton({ location: '회원가입' })}
+      >
         회원가입
       </S.RegisterButton>
-      <RegisterModal isOpen={registerOpen} onClose={onClickOtherModalOpen} />
+      <Modal isOpen={authOpen}>
+        <Funnel>
+          <Funnel.Step name="로그인">
+            <LoginModal setStep={setStep} />
+          </Funnel.Step>
+          <Funnel.Step name="회원가입">
+            <RegisterModal setStep={setStep} />
+          </Funnel.Step>
+        </Funnel>
+      </Modal>
     </>
   );
 }
