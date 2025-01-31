2d612b11d0e1413a0bac3ad5b257ea781ab32586
woo <pho9902@naver.com>
2025-01-31 22:54:19 +0900

    feat/woo(#51): 채팅 페이지 태그 생성

--- src/components/Chat/ChatPage/index.tsx
+++ src/components/Chat/ChatPage/index.tsx
@@ -1,10 +1,24 @@
 export default function ChatPage() {
   return (
     <div>
-      <div></div>
       <div>
-        <input />
-        <button>전송</button>
+        <div>{'<'}</div>
+        <h3>우리의 대화</h3>
+        <div>
+          <div>사진</div>
+          <div>정보</div>
+        </div>
+        <div>
+          <div>공유된 미디어</div>
+        </div>
+      </div>
+      <div>
+        <div>채팅창 헤더</div>
+        <div>바디</div>
+        <div>
+          <input />
+          <button>전송</button>
+        </div>
       </div>
     </div>
   );
