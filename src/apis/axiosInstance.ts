41a04b07f2b3c01f3969f91140de24a53672bfc5
zzzRYT <jinjinstar3@gmail.com>
2025-02-12 20:49:25 +0900

    deploy/LJJ(#77): test환경 http적용

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -10,6 +10,7 @@ export const commonInstance = axios.create({
   baseURL: `${import.meta.env.VITE_BASE_URL}/api/v1`,
   headers: {
     'Content-Type': 'application/json',
+    'Content-Security-Policy': 'upgrade-insecure-requests',
   },
 });
 
@@ -17,6 +18,7 @@ export const instanceToken = axios.create({
   baseURL: `${import.meta.env.VITE_BASE_URL}/api/v1`,
   headers: {
     'Content-Type': 'application/json',
+    'Content-Security-Policy': 'upgrade-insecure-requests',
   },
 });
 
