5a42a2a455190118fae3bb247bbdec4172f694ad
zzzRYT <jinjinstar3@gmail.com>
2025-02-12 21:02:49 +0900

    deploy/LJJ(#77): vite config 수정

--- vite.config.ts
+++ vite.config.ts
@@ -14,4 +14,13 @@ export default {
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
+  server: {
+    proxy: {
+      '/api/v1': {
+        target: `http://app-prod-load-balancer-1596393594.ap-northeast-2.elb.amazonaws.com`,
+        changeOrigin: true,
+        secure: false, // HTTPS → HTTP 허용
+      },
+    },
+  },
 };
