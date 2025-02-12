97e7ac7e8c60d9b311abc88cd0d497e611df13e4
zzzRYT <jinjinstar3@gmail.com>
2025-02-12 23:13:34 +0900

    deploy/LJJ(#77): config수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 // https://vite.dev/config/
 export default {
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'rewrite-middleware',
+      configureServer(serve: any) {
+        serve.middlewares.use((req: any, res: any, next: any) => {
+          if (req.url.startsWith('/nested/')) {
+            req.url = '/nested/';
+          }
+          next();
+        });
+      },
+    },
+  ],
   test: {
     browser: {
       provider: 'webdriverio',
@@ -16,9 +30,10 @@ export default {
   },
   server: {
     proxy: {
-      '/api/v1': {
+      '/api': {
         target:
           'http://app-prod-load-balancer-1596393594.ap-northeast-2.elb.amazonaws.com',
+        rewrite: (path: any) => path.replace(/^\/api/, ''),
         changeOrigin: true,
         secure: false,
       },
