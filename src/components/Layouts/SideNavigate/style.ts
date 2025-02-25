53dc1c20ac0d5a98a062300a593347815319cf05
zzzRYT <jinjinstar3@gmail.com>
2025-02-25 20:52:49 +0900

    style/LJJ(#95): 사이드 바 반응형

--- src/components/Layouts/SideNavigate/style.ts
+++ src/components/Layouts/SideNavigate/style.ts
@@ -1,3 +1,4 @@
+import { breakpoints } from '@/styles/GlobalStyles';
 import styled, { css } from 'styled-components';
 
 export const NavWrapper = styled.div`
@@ -6,6 +7,10 @@ export const NavWrapper = styled.div`
   flex-shrink: 0;
   justify-content: center;
   padding: 50px 12px 0px 12px;
+  ${breakpoints.small} {
+    position: fixed;
+    z-index: 100;
+  }
 `;
 
 export const NavItemListContainer = styled.ul`
@@ -14,6 +19,19 @@ export const NavItemListContainer = styled.ul`
   gap: 24px;
   position: fixed;
   top: 150px;
+  ${breakpoints.small} {
+    width: 100%;
+    position: fixed;
+    bottom: 0;
+    top: auto;
+    left: 0;
+    right: 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    padding: 16px;
+    background-color: ${({ theme }) => theme.bg.secondary};
+  }
 `;
 
 const itemButtonStyle = css`
